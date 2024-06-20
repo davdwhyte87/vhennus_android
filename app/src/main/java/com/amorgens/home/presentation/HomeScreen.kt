edc062466e3f81dbc6f0756c615977eb964c6d9a
David whyte <37925345+davdwhyte87@users.noreply.github.com>
2024-06-20 17:02:46 +0100

    Initial commit

---
+++ app/src/main/java/com/amorgens/home/presentation/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.amorgens.home.presentation
+
+import androidx.compose.runtime.Composable
+
+// home screen will have buttom navigation and will contains four screens
+
+@Composable
+fun HomeScreen(){
+
+}
